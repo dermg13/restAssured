aa20df60a09387e9272909945d431de90c96a0e1
dermg13 <dermg13@gmail.com>
2022-06-30 21:57:31 -0400

    added class notes and studentPractice

---
+++ src/test/java/API_Testing/API_Day1/StudentPractice/api_Activities.java
@@ -0,0 +1,51 @@
+package API_Testing.API_Day1.StudentPractice;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class api_Activities {
+    // Testing URI = https://fakerestapi.azurewebsites.net/api/v1/
+
+    // Task 1: Using Rest Assured validate the status code for endpoint /Activities
+    @Test
+    public void verifyStatusCode_Activities() {
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+// Task 2: Using Rest Assured Validate Content-Type  is application/json; charset=utf-8; v=1.0
+// for endpoint /Activities
+
+    @Test
+    public void verifyContentType_Activities() {
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities")
+                .then()
+                .contentType("application/json; charset=utf-8; v=1.0");
+    }
+// Task 1: Using Rest Assured validate the status code for endpoint /Activities/12
+
+    @Test
+    public void verifyStatusCode_ActivitiesID() {
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities/12")
+                .then()
+                .statusCode(200);
+    }
+
+    // Task 2: Using Rest Assured Validate Content-Type  is application/json; charset=utf-8; v=1.0
+// for endpoint /Activities/12
+    @Test
+    public void verifyContentType_ActivitiesID() {
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities/12")
+                .then()
+                .contentType("application/json; charset=utf-8; v=1.0");
+    }
+}
