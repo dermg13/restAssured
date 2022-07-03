cdab23d2e028ada62685004913f827a6b9533309
dermg13 <dermg13@gmail.com>
2022-07-03 12:05:19 -0400

    added create,update, and delete methods

--- src/test/java/API_Testing/workspaceRestAPI/E2E_Project.java
+++ src/test/java/API_Testing/workspaceRestAPI/E2E_Project.java
@@ -1,5 +1,6 @@
 package API_Testing.workspaceRestAPI;
 
+import com.sun.xml.bind.v2.model.core.ID;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -11,14 +12,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.http.HttpStatus.SC_OK; //Access to status code keywords/values
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class E2E_Project {
     public String path;
     String memberOf = "/workspaces/member-of";
+    Map<String, String> variables;
+    String Id;
+    String user_Id;
+    Response response;
+    String projectId;
 
     // What is a TestNG annotation that allows us to run a Test Before each Test
     @BeforeTest
-    public String setupLogInAndToken(){
+    public String setupLogInAndToken() {
         RestAssured.baseURI = "https://api.octoperf.com";
         path = "/public/users/login";
 
@@ -41,8 +49,8 @@ public class E2E_Project {
 
     // Write a test for API endpoint member-of
     @Test
-    public void verifyToken(){
-        Response response = RestAssured.given() // creating Response method
+    public void memberOf() {
+        response = RestAssured.given() // creating Response method
                 .header("Authorization", setupLogInAndToken())
                 .when()
                 .get(memberOf)
@@ -50,17 +58,89 @@ public class E2E_Project {
                 .log().all()
                 .extract().response(); // extracting response Body and start validation tests
 
+        //See the response we are getting
+//        System.out.println("-----Printing response body to verify pretty print ------ \n " + response.jsonPath().prettyPrint());
+
         // Verify status code
         Assert.assertEquals(SC_OK, response.statusCode());
         Assert.assertEquals("Default", response.jsonPath().getString("name[0]"));
 
         // TODO add tests for ID, userID, Description
-        // ID
-        Assert.assertEquals("4wsOvH0BPG-SisbyqqWo", response.jsonPath().getString("id[0]"));
-        // userId
-        Assert.assertEquals("nAsOvH0BPG-SisbyBKG7",response.jsonPath().getString("userId[0]"));
-        // description
-        Assert.assertEquals("",response.jsonPath().getString("description[0]"));
+//        // ID
+//        Assert.assertEquals("4wsOvH0BPG-SisbyqqWo", response.jsonPath().getString("id[0]"));
+//        // userId
+//        Assert.assertEquals("nAsOvH0BPG-SisbyBKG7",response.jsonPath().getString("userId[0]"));
+//        // description
+//        Assert.assertEquals("",response.jsonPath().getString("description[0]"));
+
+        // Save the is so it can be used in other requests
+        Id = response.jsonPath().get("id[0]");
+        // Save the userId so it can be used in other requests
+        user_Id = response.jsonPath().get("userId[0]");
+
+        // What can we use to store data as Key and Value
+        variables = new HashMap<String, String>();
+        variables.put("id", Id);
+        variables.put("userId", user_Id);
+    }
+
+    @Test(dependsOnMethods = {"memberOf"})
+    public void createProject() {
+        String requestBody = "{\"id\":\"\",\"created\":\"2021-03-11T06:15:20.845Z\",\"lastModified\":\"2021-03-11T06:15:20.845Z\",\"userId\":\"" + variables.get("userID") + "\",\"workspaceId\":\"" + variables.get("id") + "\",\"name\":\"testing22\",\"description\":\"testing\",\"type\":\"DESIGN\",\"tags\":[]}";
+
+        response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", setupLogInAndToken())
+                .and()
+                .body(requestBody)
+                .when()
+                .post("/design/projects")
+                .then()
+                .extract()
+                .response();
+
+        System.out.println(response.prettyPrint());
+
+        // TODO: TASK Create TestNG Assertions Name, id, userId, workspaceID
+        Assert.assertEquals("testing22", response.jsonPath().getString("name"));
+        // Using hamcrest Matchers validation
+        assertThat(response.jsonPath().getString("name"), is("testing22"));
+
+        // Store id in variable for future use
+        projectId = response.jsonPath().get("id");
+        System.out.println("New id created when creating a project" + projectId);
+    }
+    @Test(dependsOnMethods = {"memberOf", "createProject"})
+    public void updateProject(){
+        // String JSON
+        String requestBody1 = "{\"created\":1615443320845,\"description\":\"testing\",\"id\":\"" + projectId + "\",\"lastModified\":1629860121757,\"name\":\"testing Soto\",\"tags\":[],\"type\":\"DESIGN\",\"userId\":\"" + variables.get("userID") + "\",\"workspaceId\":\"" + variables.get("id") + "\"}";
+
+       response = RestAssured.given()
+                .headers("Content-Type", "application/json")
+                .header("Authorization", setupLogInAndToken())
+                .and()
+                .body(requestBody1)
+                .when()
+                .put("/design/projects/" + projectId)
+                .then()
+                .extract()
+                .response();
+        System.out.println(response.prettyPeek());
+
+        // TODO HOMEWORK add assertions for: id, name, type, userId, workspaceId, verify status code, Content-type
+    }
+    @Test(dependsOnMethods = {"memberOf", "createProject","updateProject"})
+    public void deleteProject(){
+        response = RestAssured.given()
+                .header("Authorization", setupLogInAndToken())
+                .when()
+                .delete("/design/projects" + projectId)
+                .then()
+                .extract()
+                .response();
+
+        // TODO HOMEWORK Validate status code
+
     }
 }
-A
+
